326515e478fd8874fac357858cd56a7c0fb0b2f6
Kasper Stenholt Winkler <kwinkl18@student.aau.dk>
2019-05-27 20:54:52 +0200

    Added controllers for Parts and BusinessProfiles

---
+++ AGP2P/Controllers/BusinessProfilesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AGP2P.Data;
+using AGP2P.Models;
+
+namespace AGP2P.Controllers
+{
+    public class BusinessProfilesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BusinessProfilesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BusinessProfiles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BusinessProfiles.ToListAsync());
+        }
+
+        // GET: BusinessProfiles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var businessProfile = await _context.BusinessProfiles
+                .FirstOrDefaultAsync(m => m.BusinessProfileId == id);
+            if (businessProfile == null)
+            {
+                return NotFound();
+            }
+
+            return View(businessProfile);
+        }
+
+        // GET: BusinessProfiles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BusinessProfiles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BusinessProfileId,BusinessName,Country,Town,Address,PhoneNumber")] BusinessProfile businessProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(businessProfile);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(businessProfile);
+        }
+
+        // GET: BusinessProfiles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var businessProfile = await _context.BusinessProfiles.FindAsync(id);
+            if (businessProfile == null)
+            {
+                return NotFound();
+            }
+            return View(businessProfile);
+        }
+
+        // POST: BusinessProfiles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BusinessProfileId,BusinessName,Country,Town,Address,PhoneNumber")] BusinessProfile businessProfile)
+        {
+            if (id != businessProfile.BusinessProfileId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(businessProfile);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BusinessProfileExists(businessProfile.BusinessProfileId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(businessProfile);
+        }
+
+        // GET: BusinessProfiles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var businessProfile = await _context.BusinessProfiles
+                .FirstOrDefaultAsync(m => m.BusinessProfileId == id);
+            if (businessProfile == null)
+            {
+                return NotFound();
+            }
+
+            return View(businessProfile);
+        }
+
+        // POST: BusinessProfiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var businessProfile = await _context.BusinessProfiles.FindAsync(id);
+            _context.BusinessProfiles.Remove(businessProfile);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BusinessProfileExists(int id)
+        {
+            return _context.BusinessProfiles.Any(e => e.BusinessProfileId == id);
+        }
+    }
+}
---
+++ AGP2P/Controllers/PartsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AGP2P.Data;
+using AGP2P.Models;
+
+namespace AGP2P.Controllers
+{
+    public class PartsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PartsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Parts
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Parts.Include(p => p.BusinessProfile);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Parts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _context.Parts
+                .Include(p => p.BusinessProfile)
+                .FirstOrDefaultAsync(m => m.PartId == id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+
+            return View(part);
+        }
+
+        // GET: Parts/Create
+        public IActionResult Create()
+        {
+            ViewData["BusinessProfileId"] = new SelectList(_context.BusinessProfiles, "BusinessProfileId", "BusinessProfileId");
+            return View();
+        }
+
+        // POST: Parts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PartId,PartName,Amount,BusinessProfileId")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(part);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BusinessProfileId"] = new SelectList(_context.BusinessProfiles, "BusinessProfileId", "BusinessProfileId", part.BusinessProfileId);
+            return View(part);
+        }
+
+        // GET: Parts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _context.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+            ViewData["BusinessProfileId"] = new SelectList(_context.BusinessProfiles, "BusinessProfileId", "BusinessProfileId", part.BusinessProfileId);
+            return View(part);
+        }
+
+        // POST: Parts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PartId,PartName,Amount,BusinessProfileId")] Part part)
+        {
+            if (id != part.PartId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(part);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PartExists(part.PartId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BusinessProfileId"] = new SelectList(_context.BusinessProfiles, "BusinessProfileId", "BusinessProfileId", part.BusinessProfileId);
+            return View(part);
+        }
+
+        // GET: Parts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var part = await _context.Parts
+                .Include(p => p.BusinessProfile)
+                .FirstOrDefaultAsync(m => m.PartId == id);
+            if (part == null)
+            {
+                return NotFound();
+            }
+
+            return View(part);
+        }
+
+        // POST: Parts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var part = await _context.Parts.FindAsync(id);
+            _context.Parts.Remove(part);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PartExists(int id)
+        {
+            return _context.Parts.Any(e => e.PartId == id);
+        }
+    }
+}
