f168b13f122b79c1c81fea96f0175a3729888c5c
Mads Smed <mthoma18@student.aau.dk>
2019-05-27 21:54:00 +0200

    Added ApplicationUser as type parameter in IdentityDbContext

--- AGP2P/Data/ApplicationDbContext.cs
+++ AGP2P/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AGP2P.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
