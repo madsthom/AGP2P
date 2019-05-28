4d780817b696d21e713f76a12d46e65ebfd13f59
Mads Smed <mads.th@hotmail.com>
2019-05-28 16:06:20 +0200

    Added TestViewModel

--- AGP2P/Controllers/HomeController.cs
+++ AGP2P/Controllers/HomeController.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 using AGP2P.Data;
 using Microsoft.AspNetCore.Mvc;
 using AGP2P.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AGP2P.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -23,7 +31,9 @@ namespace AGP2P.Controllers
 
         public IActionResult TestView()
         {
-            return View();
+            List<Part> partList = _context.Parts.ToList();
+
+            return View(partList);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ AGP2P/Models/TestViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AGP2P.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AGP2P.Models
+{
+    public class TestViewModel
+    {
+        private ApplicationDbContext _context;
+        public TestViewModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private BusinessProfile _businessProfiles;
+
+        public List<BusinessProfile> BusinessProfiles
+        {
+            get { return _context.Parts.Include(bp => bp.BusinessProfile).ToList(); }
+            set => throw new System.NotImplementedException();
+        }
+
+        private Part _parts;
+        public List<Part> Parts { get; set; }
+    }
+}
